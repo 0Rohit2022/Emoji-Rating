e3d23321963cc73a784c44a5f0b2a65b58a39d08
Rohit kumar <112196544+0Rohit2022@users.noreply.github.com>
2022-10-01 20:18:36 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+const starsEl = document.querySelectorAll(".fa-star");
+ 
+const emojis = document.querySelectorAll(".far");
+
+const colorArrays = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+updateRating(0)
+
+starsEl.forEach((starEl , index) => {
+    starEl.addEventListener("click", () => {
+        updateRating(index);
+    });
+});
+
+function updateRating(index) {
+    starsEl.forEach((starEl , idx) => {
+        if(idx < index + 1){
+            starEl.classList.add("active");
+        }
+        else {
+            starEl.classList.remove("active");
+        }
+    });
+    emojis.forEach((emojiEl) => {
+         emojiEl.style.transform = `translateX(-${index * 50}px)`;
+        emojiEl.style.color = colorArrays[index];
+
+       
+    })
+
+
+
+}
+
